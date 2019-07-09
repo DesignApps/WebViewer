a94566c16b3c5f3480062f66dcf19aefd3c1c4c9
Amy Graff <amygraff.psychemath@gmail.com>
2019-07-09 11:00:24 -0500

    Updated activity_main and others to match gridLayout

--- app/src/main/java/com/onewhocreates/webviewer/MainActivity.java
+++ app/src/main/java/com/onewhocreates/webviewer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.onewhocreates.webviewer;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
